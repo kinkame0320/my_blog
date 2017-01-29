d582ee0a020f04db1507817af2e00a2d787f0a84
Takuma Toyama <ttoyama@sigmaxyz.com>
2017-01-29 08:57:32 +0000

    6時なので一回Push

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 Faker::Config.locale = :ja
 
 100.times do |index|
+    number = index.to_i
     Post.create(
         title: Faker::StarWars.character,
         body: Faker::Food.ingredient,
-        category: Faker::Color.color_name
+        category: Faker::Color.color_name,
+        author_id: number % 5
         )
+
 end
